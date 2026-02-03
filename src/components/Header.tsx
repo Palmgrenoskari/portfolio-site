8696093f7acea44934f04183447481e0236fa362
Oskari Palmgren <jokpal@utu.fi>
2026-02-03 15:57:02 +0200

    update header description

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
         <Link href="/" className="flex items-center space-x-2">
           <LogoWithName
             name="Oskari Palmgren"
-            description="MSc Student & Aspiring Dev"
+            description="Software Developer & MSc Student"
           />
         </Link>
 
